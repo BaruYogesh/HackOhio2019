e571f36756dabd12e9b3128a3ad220130f80ade7
Baru Yogesh <bhargav.yogesh@gmail.com>
2019-11-02 13:54:22 -0400

    fixed text analysis

--- src/TextAnalysis.js
+++ src/TextAnalysis.js
@@ -1,14 +1,14 @@
 const CognitiveServicesCredentials = require("@azure/ms-rest-js");
 const TextAnalyticsAPIClient = require("@azure/cognitiveservices-textanalytics");
 
-const key_var = 'TEXT_ANALYTICS_SUBSCRIPTION_KEY';
+const key_var = '\'TEXT_ANALYTICS_SUBSCRIPTION_KEY\'';
 //const key_var = 'c93eee6b9dab47299eff9ebae6ced048';
 if (!process.env[key_var]) {
     throw new Error('please set/export the following environment variable: ' + key_var);
 }
 const subscription_key = process.env[key_var];
 
-const endpoint_var = 'TEXT_ANALYTICS_ENDPOINT';
+const endpoint_var = '\'TEXT_ANALYTICS_ENDPOINT\'';
 //const endpoint_var = 'https://eastus.api.cognitive.microsoft.com/';
 if (!process.env[endpoint_var]) {
     throw new Error('please set/export the following environment variable: ' + endpoint_var);
